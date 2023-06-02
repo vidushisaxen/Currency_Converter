e4516893676bb47dd66e7f94a0b19b4cb15fe91d
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-06-02 20:00:21 +0530

    main

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
 const dropList= document.querySelectorAll(".drop-list select")
+getButton = document.querySelector("form button");
 
 for(let i = 0;i < dropList.length ; i++){
     for(currency_code in country_code){
-        console.log(currency_code)
+       let selected;
+       if(i==0){
+        selected = currency_code == "USD"? "selected" : "";
+       }else if(i==1){
+        selected = currency_code == "NPR" ? "selected" : "";
+       }
+
+        let optionTag =`<option value="${currency_code}">${currency_code}</option>`;
+        dropList[i].insertAdjacentHTML("beforeend",optionTag);
+
     }
 }
+
+  getButton.addEventListener("click", e=>{
+    e.preventDefault();
+    getExchangeRate();
+  });
+
+  function getExchangeRate(){
+    const amount = document.querySelector("")
+  }
